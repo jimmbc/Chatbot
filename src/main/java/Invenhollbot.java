7a1d55e8d7b304b550744833edfe40f62a320d0c
Jimena Choque <jimenachoque.bc@gmail.com>
2019-10-05 04:30:15 -0400

    Saludo 1

--- src/main/java/Invenhollbot.java
+++ src/main/java/Invenhollbot.java
@@ -1,10 +1,25 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Invenhollbot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
         System.out.println(update.getMessage().getText());
         System.out.println(update.getMessage().getFrom().getFirstName());
+
+        SendMessage message = new SendMessage();
+        String command=update.getMessage().getText();
+        if (command.equals("/saludame")){
+            System.out.println(update.getMessage().getFrom().getFirstName());
+            message.setText("Hola "+update.getMessage().getFrom().getFirstName()+ " me alegra poder ayudarte");
+        }
+        message.setChatId(update.getMessage().getChatId());
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 
     public String getBotUsername() {
